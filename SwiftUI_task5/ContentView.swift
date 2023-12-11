126c58b92c045ce391843057aec970dd4d770156
Evgenii Mikhailov <e.mikhailov@outlook.com>
2023-12-11 16:17:13 +0300

    done

--- SwiftUI_task5/ContentView.swift
+++ SwiftUI_task5/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var location = CGPoint(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height/2)
+    let colors = [Color.white, .pink, .yellow, .black]
+    let colorsBack = [Color.black, .white, .black, .white]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+
+            VStack(spacing:0) {
+                ForEach(colorsBack, id: \.self) { color in
+                    Rectangle()
+                        .foregroundStyle(color)
+                }
+            }
+
+            ZStack {
+
+                VStack(spacing:0) {
+                    ForEach(colors, id: \.self) { color in
+                        Rectangle()
+                            .foregroundStyle(color)
+                    }
+                }
+
+                RoundedRectangle(cornerSize: CGSize(width: 20, height: 20)).frame(width: 100, height: 100)
+                    .position(location)
+                    .gesture(DragGesture()
+                        .onChanged { gesture in
+                            self.location = gesture.location
+                        })
+                    .blendMode(.destinationOut)
+            }
+            .compositingGroup()
         }
-        .padding()
+        .ignoresSafeArea(.all)
     }
 }
 
